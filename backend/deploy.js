ed187f370193bddbf46895c9970b048c9ef882f3
giorgiabes <abesadze.george@gmail.com>
2024-05-23 13:27:35 +0400

    add deploy.js

---
+++ backend/deploy.js
@@ -0,0 +1,9 @@
+const { execSync } = require("child_process");
+
+const commitMessage = process.argv[2] || "uibuild";
+
+execSync("npm run build:ui", { stdio: "inherit" });
+execSync(
+  `cd ../ && git add . && git commit -m "${commitMessage}" && git push -f`,
+  { stdio: "inherit" }
+);
