1c14bc332348da2a81ed8a80cfe6ace54ac22642
giorgiabes <abesadze.george@gmail.com>
2024-05-30 23:20:31 +0400

    supertest

--- backend/mongo.js
+++ backend/mongo.js
@@ -7,7 +7,7 @@ if (process.argv.length < 3) {
 
 const password = process.argv[2];
 
-const url = `mongodb+srv://admin:${password}@cluster0.3jssfuh.mongodb.net/noteApp?retryWrites=true&w=majority&appName=Cluster0`;
+const url = `mongodb+srv://admin:${password}@cluster0.3jssfuh.mongodb.net/testNoteApp?retryWrites=true&w=majority&appName=Cluster0`;
 
 mongoose.set("strictQuery", false);
 
@@ -20,20 +20,20 @@ const noteSchema = new mongoose.Schema({
 
 const Note = mongoose.model("Note", noteSchema);
 
-// const note = new Note({
-//   content: "GET and POST are the most important methods of HTTP protocol",
-//   important: true,
-// });
+const note = new Note({
+  content: "MERN stack is fun.",
+  important: true,
+});
 
-// note.save().then((result) => {
-//   console.log("note saved");
-//   console.log("result is:", result);
-//   mongoose.connection.close();
-// });
-
-Note.find({ important: true }).then((result) => {
-  result.forEach((note) => {
-    console.log(note);
-  });
+note.save().then((result) => {
+  console.log("note saved");
+  console.log("result is:", result);
   mongoose.connection.close();
 });
+
+// Note.find({ important: true }).then((result) => {
+//   result.forEach((note) => {
+//     console.log(note);
+//   });
+//   mongoose.connection.close();
+// });
